c0a7703a705725a242bd0d9a467921d57579682c
Erik van Zijst <erik.van.zijst@gmail.com>
2014-09-16 08:28:43 -0700

    Optimized 15 to run on Python.

--- q15.py
+++ q15.py
@@ -1,15 +1,18 @@
-xx, yy = 4, 4
+xx, yy = 20, 20
+steps = {}
 
 
-def step(x, y, total=0):
-    if x < 13 and x == y:
-        print '(%d, %d)' % (x, y)
+def step(x, y):
+    if (x, y) in steps:
+        return steps[(x, y)]
     if x == xx and y == yy:
-        return total + 1
+        return 1
+    total = 0
     if x < xx:
-        total = step(x + 1, y, total)
+        total += step(x + 1, y)
     if y < yy:
-        total = step(x, y + 1, total)
+        total += step(x, y + 1)
+    steps[(x, y)] = total
     return total
 
 print step(0, 0)
