63ddfbf10283443227042843b598deab58dcd983
Erik van Zijst <erik.van.zijst@gmail.com>
2014-09-16 23:55:20 -0700

    Minor cleanup.

--- q19.py
+++ q19.py
@@ -1,25 +1,24 @@
 from itertools import cycle, islice
 
 months = [
-    lambda year: 31,  # jan
-    lambda year: 29 if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0) else 28,  # feb
-    lambda year: 31,  # mar
-    lambda year: 30,  # apr
-    lambda year: 31,  # may
-    lambda year: 30,  # jun
-    lambda year: 31,  # jul
-    lambda year: 31,  # aug
-    lambda year: 30,  # sept
-    lambda year: 31,  # oct
-    lambda year: 30,  # nov
-    lambda year: 31,  # dec
+    lambda y: 31,  # jan
+    lambda y: 29 if (y % 4 == 0 and y % 100 != 0 or y % 400 == 0) else 28,  # feb
+    lambda y: 31,  # mar
+    lambda y: 30,  # apr
+    lambda y: 31,  # may
+    lambda y: 30,  # jun
+    lambda y: 31,  # jul
+    lambda y: 31,  # aug
+    lambda y: 30,  # sept
+    lambda y: 31,  # oct
+    lambda y: 30,  # nov
+    lambda y: 31,  # dec
 ]
 days = cycle(range(7))  # 5 is sun
 count = 0
 
 for year in xrange(1901, 2001):
-    for m, month in enumerate(months):
-        md = list(islice(days, month(year)))
-        if md[0] == 5:
+    for month in months:
+        if list(islice(days, month(year)))[0] == 5:
             count += 1
 print count
